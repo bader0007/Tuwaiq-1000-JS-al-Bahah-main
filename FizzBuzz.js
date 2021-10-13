6aa5946fcaccabe8054169da65df520a812e98dd
bader0007 <89877474+bader0007@users.noreply.github.com>
2021-10-13 23:07:29 +0300

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,15 @@
+let num1 = 15
+if (num1 % 3 == 0 && num1 % 5 == 0) {
+    console.log("FizzBuzz")
+}
+else if (num1 % 3 == 0) {
+    console.log("Fizz")
+}
+
+else if (num1 % 5 == 0) {
+    console.log("Buzz")
+}
+
+else {
+    console.log(num1)
+}
